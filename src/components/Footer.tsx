8a0a7349e43eb44d65bb062127f7c94ac565d3d8
jhoniernem <jhonny30ca@gmail.com>
2023-07-10 12:10:40 -0500

    ...

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="mb-8 rounded-md bg-text-primary p-4 text-gray-100">
+      hola soy el footer
+    </footer>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
+import { Footer } from '~/components/Footer'
 import { MainContainer } from '~/components/Main'
 import { NavBar } from '~/components/NavBar'
 
 export default function Home() {
-
   return (
     <>
       <Head>
@@ -11,8 +11,9 @@ export default function Home() {
         <meta name="description" content="Frontend Developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MainContainer>
-        <NavBar />
+      <MainContainer className="overflow-hidden" Navbar={<NavBar />}>
+        <main className="h-screen p-4">holas 2</main>
+        <Footer />
       </MainContainer>
     </>
   )
