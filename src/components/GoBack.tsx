e75105e01f9f26ffd6555ad0ad91b33fc692a958
jhoniernem <jhonny30ca@gmail.com>
2023-07-11 17:36:21 -0500

    Add children:react node

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,12 +1,19 @@
+import type { ReactNode } from 'react'
 import Link from 'next/link'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
 type GoBackProps = {
   home?: boolean
+  children?: ReactNode
 }
 
-export function GoBack({ home = false }: GoBackProps) {
-  return <Link href={home ? '/' : '..'}>
-    <XMarkIcon className='w-8 h-8'/>
-  </Link>
+export function GoBack({ children, home = false }: GoBackProps) {
+  return (
+    <Link
+      href={home ? '/' : '..'}
+      className="flex h-9 w-9 items-center justify-center  rounded-full pl-1 py-2 pr-2 hover:bg-bg-secondary hover:shadow"
+    >
+      {children || <XMarkIcon className="h-full w-full" />}
+    </Link>
+  )
 }
