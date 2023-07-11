7864d1ad9ff0212f52f36afd9b8bb7898074f6ef
jhoniernem <jhonny30ca@gmail.com>
2023-07-11 14:51:29 -0500

    implement react hot toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from 'next/app'
 import { api } from '~/utils/api'
 import '~/styles/globals.css'
 import { ThemeProvider } from '@material-tailwind/react'
+import { Toaster } from 'react-hot-toast'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
+      <Toaster position='bottom-center' />
       <Component {...pageProps} />
     </ThemeProvider>
   )
