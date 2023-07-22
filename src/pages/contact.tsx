cdef129d930721144ed380e75e0447465080d627
jhoniernem <jhonny30ca@gmail.com>
2023-07-22 17:03:29 -0500

    add head title

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import { MainContainer } from '~/components/Main'
 import { NavBar } from '~/components/NavBar'
 
 export default function ContactMe() {
   return (
     <>
+      <Head>
+        <title>Sourer Dev | Contact me</title>
+        <meta name="description" content="Frontend Developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <MainContainer Navbar={<NavBar />} className="p-6">
         Contact me
       </MainContainer>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,16 @@
+import Head from 'next/head'
 import { MainContainer } from '~/components/Main'
 import { NavBar } from '~/components/NavBar'
 
 export default function ProjectsPage() {
   return (
     <>
-      <MainContainer className='p-6' Navbar={<NavBar />}>
+      <Head>
+        <title>Sourer Dev | Projects</title>
+        <meta name="description" content="Frontend Developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MainContainer className="p-6" Navbar={<NavBar />}>
         <p>This is a project page</p>
       </MainContainer>
     </>
