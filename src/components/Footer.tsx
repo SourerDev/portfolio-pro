7120e3a0a44275e99eb81eec39168aaf8aad9bfa
jhoniernem <jhonny30ca@gmail.com>
2023-07-25 16:06:03 -0500

    Add styles footer max w 5xl and margin x auto

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { LoginAdmin } from '~/modules/auth/LoginAdmin'
 
 export function Footer() {
   return (
-    <footer className="mb-8 flex justify-between md:rounded-md bg-text-primary p-8 text-gray-100 dark:bg-bg-primary dark:text-text-primary">
+    <footer className="mx-auto mb-8 flex max-w-5xl justify-between bg-text-primary p-8 text-gray-100 dark:bg-bg-primary dark:text-text-primary md:rounded-md">
       <h2 className="text-[1.3rem] font-semibold">Sourer Dev</h2>
       <LoginAdmin />
     </footer>
