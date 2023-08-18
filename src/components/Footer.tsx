f0a9ed6c9ae74ad39a4d6fd87ed5a89450e4d83d
jhoniernem <jhonny30ca@gmail.com>
2023-08-18 10:58:11 -0500

    remove - button for auth

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,7 @@
-import { LoginAdmin } from '~/modules/auth/LoginAdmin'
-
 export function Footer() {
   return (
     <footer className="mx-auto mb-8 flex max-w-5xl justify-between bg-background-dk p-8 text-text-dk dark:bg-background dark:text-text md:rounded-md">
       <h2 className="text-[1.2rem] font-semibold">Sourer Dev</h2>
-      <LoginAdmin />
     </footer>
   )
 }
