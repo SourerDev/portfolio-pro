c258bbe4e5fd4b75b1f25ce10c0dba70cb357da3
jhoniernem <jhonny30ca@gmail.com>
2023-09-04 21:35:18 -0500

    refactor variables

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,44 @@
 import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
 import { Typography } from '@material-tailwind/react'
 import Head from 'next/head'
-import { useState } from 'react'
+import { useReducer } from 'react'
 import { MainInput, MainTextArea } from '~/components/Input'
 import { ContainerPage } from '~/components/container_page'
 
+enum ActionTypes {
+  SET_NAME,
+  SET_EMAIL,
+  SET_MESSAGE,
+  RESET,
+}
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+}
+
+function reducer(
+  state: typeof initialState,
+  action: { payload: string; type: ActionTypes }
+) {
+  switch (action.type) {
+    case ActionTypes.SET_NAME:
+      return { ...state, name: action.payload }
+    case ActionTypes.SET_EMAIL:
+      return { ...state, email: action.payload }
+    case ActionTypes.SET_MESSAGE:
+      return { ...state, message: action.payload }
+    case ActionTypes.RESET:
+      return { ...initialState }
+
+    default:
+      throw new Error()
+  }
+}
+
 export default function ContactMe() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <>
@@ -29,22 +59,31 @@ export default function ContactMe() {
             <MainInput
               label="Name"
               placeholder="your name"
-              value={name}
-              onChange={({ target }) => setName(target.value)}
+              value={state.name}
+              onChange={({ target }) =>
+                dispatch({ payload: target.value, type: ActionTypes.SET_NAME })
+              }
             />
             <MainInput
               label="Email"
               placeholder="example@example.ex"
-              value={email}
-              onChange={({ target }) => setEmail(target.value)}
+              value={state.email}
+              onChange={({ target }) =>
+                dispatch({ payload: target.value, type: ActionTypes.SET_EMAIL })
+              }
             />
           </div>
           <div className="my-5">
             <MainTextArea
               label="Message"
               placeholder="Enter your message here"
-              value={message}
-              onChange={({ target }) => setMessage(target.value)}
+              value={state.message}
+              onChange={({ target }) =>
+                dispatch({
+                  payload: target.value,
+                  type: ActionTypes.SET_MESSAGE,
+                })
+              }
             />
           </div>
           <button className="group flex items-center gap-2 self-end rounded-md bg-primary py-3 pl-4 pr-5 font-medium text-text-dk hover:scale-105 hover:shadow-md">
