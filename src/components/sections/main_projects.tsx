522c22b1b92bcfb90b332c583f7a50cbe3bb1171
SourerDev <jhonny30ca@gmail.com>
2024-01-05 09:45:21 -0500

    add data for develop

--- src/components/sections/main_projects.tsx
+++ src/components/sections/main_projects.tsx
@@ -1,15 +1,15 @@
+import { projects } from '~/utils/data.json'
+
 export function MainProjectsSection() {
   return (
     <section className="space-y-6">
       <h2 className="mt-4 text-center text-2xl font-bold uppercase">
         Portfolio
       </h2>
-      <div
-        className="grid grid-cols-2 gap-4"
-      >
-        {[1, 2, 3, 4, 5].map((num) => (
-          <div className="border-2 h-80 grid place-content-center" key={num}>
-            {num}
+      <div className="grid grid-cols-2 gap-4">
+        {projects.map(({ name }, i) => (
+          <div className="grid h-80 place-content-center border-2" key={i}>
+            {name}
           </div>
         ))}
       </div>
