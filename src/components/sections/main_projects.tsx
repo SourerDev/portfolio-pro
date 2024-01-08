7ff9c4939b20ae2a8feb8b8c28fcb3a69b490276
SourerDev <jhonny30ca@gmail.com>
2024-01-07 22:47:08 -0500

    style

--- src/components/sections/main_projects.tsx
+++ src/components/sections/main_projects.tsx
@@ -1,3 +1,4 @@
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 import { projects } from '~/utils/data.json'
 
 export function MainProjectsSection() {
@@ -6,10 +7,19 @@ export function MainProjectsSection() {
       <h2 className="mt-4 text-center text-2xl font-bold uppercase">
         Portfolio
       </h2>
-      <div className="grid grid-cols-2 gap-1 sm:gap-4">
-        {projects.map(({ name }, i) => (
-          <div className="grid h-80 place-content-center border-2" key={i}>
-            {name}
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-4">
+        {projects.map(({ name, images }, i) => (
+          <div
+            className="group relative grid h-80 place-content-center overflow-hidden bg-center bg-cover hover:backdrop-blur-3xl"
+            key={i}
+            style={{
+              backgroundImage: `url(${images.main})`,
+            }}
+          >
+            <div className="absolute inset-x-0 top-[75%] h-1/2 bg-primary p-4 transition-all duration-500 ease-in-out group-hover:top-[50%] rounded-t-xl">
+              <ArrowTopRightOnSquareIcon className="block aspect-square w-8" />
+              {name}
+            </div>
           </div>
         ))}
       </div>
