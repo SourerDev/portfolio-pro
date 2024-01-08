5cc8236c3dadae9cb5a7c957350c043d20b95a62
SourerDev <jhonny30ca@gmail.com>
2024-01-08 17:11:12 -0500

    style ~

--- src/components/sections/main_projects.tsx
+++ src/components/sections/main_projects.tsx
@@ -1,4 +1,8 @@
-import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
+import {
+  ArrowLeftOnRectangleIcon,
+  ArrowTopRightOnSquareIcon,
+  ChevronRightIcon,
+} from '@heroicons/react/24/outline'
 import { projects } from '~/utils/data.json'
 
 export function MainProjectsSection() {
@@ -10,19 +14,31 @@ export function MainProjectsSection() {
       <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-4">
         {projects.map(({ name, images }, i) => (
           <div
-            className="group relative grid h-80 place-content-center overflow-hidden bg-center bg-cover hover:backdrop-blur-3xl"
+            className="group relative grid h-80 place-content-center overflow-hidden bg-cover bg-center hover:backdrop-blur-3xl"
             key={i}
             style={{
               backgroundImage: `url(${images.main})`,
             }}
           >
-            <div className="absolute inset-x-0 top-[75%] h-1/2 bg-primary p-4 transition-all duration-500 ease-in-out group-hover:top-[50%] rounded-t-xl">
-              <ArrowTopRightOnSquareIcon className="block aspect-square w-8" />
-              {name}
+            <div className="absolute inset-x-0 top-[75%] h-1/2 rounded-t-xl bg-primary p-4 text-white transition-all duration-500 ease-in-out group-hover:top-[50%]">
+              <div className="flex items-center justify-between">
+                <p className="text-xl font-semibold ">{name}</p>
+                <div className="rounded-md p-2 transition-all ease-in-out hover:scale-110 hover:bg-accent hover:shadow">
+                  <ArrowTopRightOnSquareIcon className=" aspect-square w-8" />
+                </div>
+              </div>
             </div>
           </div>
         ))}
       </div>
+      <p className=" text-right">
+        <span className='hover:font-semibold p-3 rounded-md hover:text-primary'>
+          View More{' '}
+          {
+            <ChevronRightIcon className="inline-block aspect-square w-5 align-middle " />
+          }{' '}
+        </span>
+      </p>
     </section>
   )
 }
