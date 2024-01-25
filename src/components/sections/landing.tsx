600f344ed87f4260a17867aab49c2455c0dadf07
SourerDev <jhonny30ca@gmail.com>
2024-01-25 18:51:20 -0500

    add min and max size

--- src/components/sections/landing.tsx
+++ src/components/sections/landing.tsx
@@ -6,7 +6,7 @@ import { Icon } from '~/components/Icons'
 
 export function LandingSection() {
   return (
-    <main className="relative min-h-[90vh]">
+    <main className="relative h-screen max-h-[610px] min-h-[500px]">
       <div className=" grid place-content-center">
         <Image
           className="aspect-square w-[168px] rounded-sm object-cover md:h-[200px] md:w-[200px]"
@@ -16,10 +16,12 @@ export function LandingSection() {
           alt="Owner"
         />
       </div>
-      <div className="md:w-3/4 text-center mt-2">
+      <div className="mt-2 text-center md:w-3/4">
         <span className="hidden">Hello</span>
-        <h1 className="text-primary text-[48px] font-bold">I&lsquo;m Yhonier</h1>
-        <h2 className='text-2xl font-semibold'>Frontend Developer</h2>
+        <h1 className="text-[48px] font-bold text-primary">
+          I&lsquo;m Yhonier
+        </h1>
+        <h2 className="text-2xl font-semibold">Frontend Developer</h2>
         <div className="mt-5 ">
           <Typography variant="paragraph">
             I love working with typescript and react, I have experience creating
